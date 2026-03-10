1af65c145cdf16b42e3fc89b7b55696d520d5ae5
Karan Singh <37649078+KaranSinghG@users.noreply.github.com>
2026-03-10 09:01:38 +0530

    Implement containsDuplicate method
    Removed isAnagram method and added containsDuplicate method to check for duplicates in an array.

--- arrays-hashing/contains-duplicate/ContainsDuplicate.java
+++ arrays-hashing/contains-duplicate/ContainsDuplicate.java
@@ -1,21 +1,13 @@
 class ContainsDuplicate {
-    public boolean isAnagram(String s, String t) {
-        int[] map = new int[26];
-        if(s.length()!=t.length()){
-            return false;
-        }
-        for(char c : s.toCharArray()){
-            map[c-'a']++;
-        }
-        for(char c : t.toCharArray()){
-            if(--map[c-'a']<0)
-                return false;
-        }
-        for(int i=0;i<26;i++){
-            if(map[i]!=0){
-                return false;
+    public boolean containsDuplicate(int[] nums) {
+        Set s = new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(s.contains(nums[i])){
+                return true;
+            } else {
+                s.add(nums[i]);
             }
-        } 
-        return true;
+        }
+        return false;
     }
 }
